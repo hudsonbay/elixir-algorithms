41238e76e1ab8d1e9925e1b6b0017416eac89f5d
hudsonbay <manuelmenendezalfonso@gmail.com>
2020-06-17 18:04:38 -0400

    added test_watch right dependency

--- mix.exs
+++ mix.exs
@@ -22,8 +22,8 @@ defmodule ElixirTraining.MixProject do
   defp deps do
     [
       {:earmark, "~> 0.1", only: :dev}, # a pure-Elixir Markdown converter
-      {:ex_doc, "~> 0.11", only: :dev} # for generating documentation
-      {:mix_test_watch, "~> 0.3", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.11", only: :dev}, # for generating documentation
+      {:mix_test_watch, "~> 1.0.2", only: :dev}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
